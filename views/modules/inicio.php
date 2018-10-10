bb101421675cb6ef0a921b4fb7c7d919c541418c
E-studio <rickyurbina@gmail.com>
2018-10-10 11:31:04 -0600

    Se incluyo alertas en inicio de administrador

--- views/modules/inicio.php
+++ views/modules/inicio.php
@@ -20,7 +20,7 @@ if ($_SESSION["rol"] == 3){
     </div><!-- /.container-fluid-->
   </div>
 
-<?php 
+<?php
 include "views/modules/footer.php";
 }
 elseif ($_SESSION["rol"] == 2){
@@ -40,7 +40,7 @@ elseif ($_SESSION["rol"] == 2){
     </div><!-- /.container-fluid-->
   </div>
 
-<?php 
+<?php
 include "views/modules/footer.php";
 }
 elseif ($_SESSION["rol"] == 1){
@@ -59,16 +59,70 @@ elseif ($_SESSION["rol"] == 1){
     </div><!-- /.container-fluid-->
   </div>
 
-<?php 
+<?php
 include "views/modules/footer.php";
 }
-elseif($_SESSION["rol"] == 0){ 
+elseif($_SESSION["rol"] == 0){
 
 //  ==========================             Pantalla de Administrador =======================================
 
  include "navAdmin.php"; ?>
   <div class="content-wrapper">
     <div class="container-fluid">
+
+      <div class="row">
+        <div class="col-xl-3 col-sm-6 mb-3">
+          <div class="card text-white bg-primary o-hidden h-100">
+            <div class="card-body">
+              <div class="card-body-icon">
+                <i class="fa fa-fw fa-comments"></i>
+              </div>
+              <div class="mr-5">26 New Messages!</div>
+            </div>
+            <a class="card-footer text-white clearfix small z-1" href="#">
+              <span class="float-left">View Details</span>
+              <span class="float-right">
+                <i class="fa fa-angle-right"></i>
+              </span>
+            </a>
+          </div>
+        </div>
+
+        <div class="col-xl-3 col-sm-6 mb-3">
+          <div class="card text-white bg-warning o-hidden h-100">
+            <div class="card-body">
+              <div class="card-body-icon">
+                <i class="fa fa-fw fa-list"></i>
+              </div>
+              <div class="mr-5">11 New Tasks!</div>
+            </div>
+            <a class="card-footer text-white clearfix small z-1" href="#">
+              <span class="float-left">View Details</span>
+              <span class="float-right">
+                <i class="fa fa-angle-right"></i>
+              </span>
+            </a>
+          </div>
+        </div>
+
+        <div class="col-xl-3 col-sm-6 mb-3">
+          <div class="card text-white bg-success o-hidden h-100">
+            <div class="card-body">
+              <div class="card-body-icon">
+                <i class="fa fa-fw fa-shopping-cart"></i>
+              </div>
+              <div class="mr-5">123 New Orders!</div>
+            </div>
+            <a class="card-footer text-white clearfix small z-1" href="#">
+              <span class="float-left">View Details</span>
+              <span class="float-right">
+                <i class="fa fa-angle-right"></i>
+              </span>
+            </a>
+          </div>
+        </div>
+
+      </div> <!-- Row  -->
 
       <!-- Area Chart Example-->
       <div class="row">
@@ -93,7 +147,7 @@ elseif($_SESSION["rol"] == 0){
       <div class="row">
         <div class="col-sm-2">
         </div>
-        
+
         <div class="col-lg-6">
           <!-- Example Bar Chart Card-->
           <div class="card mb-3">
@@ -107,7 +161,7 @@ elseif($_SESSION["rol"] == 0){
                 <div class="col-sm-4 text-center my-auto">
                   <div class="h4 mb-0 text-primary">$##,###.##</div>
                   <div class="small text-muted">Best Month</div>
-                  
+
                 </div>
               </div>
             </div>
@@ -115,7 +169,7 @@ elseif($_SESSION["rol"] == 0){
           </div>
         </div>
 
-       
+
       </div><!-- row -->
     <div class="row">
       <!--<div class="col-lg-4">
@@ -128,7 +182,7 @@ elseif($_SESSION["rol"] == 0){
             </div>
             <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
           </div>
-          
+
         </div>
     </div>--><!-- row -->
 
